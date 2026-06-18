56dd17cc8884ef82bb788596400d57a36b47b3da
anurag jaiswal <anuraggjaiswal@gmail.com>
2026-06-18 16:05:33 +0530

    Resolved nextJs config issue during deployement

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    resolveAlias: {
+      "@prisma/client/runtime/client": "./node_modules/@prisma/client/runtime/client.js",
+      "@prisma/client/runtime/query_compiler_fast_bg.postgresql.mjs": "./node_modules/@prisma/client/runtime/query_compiler_fast_bg.postgresql.mjs",
+      "@prisma/client/runtime/query_compiler_fast_bg.postgresql.wasm-base64.mjs": "./node_modules/@prisma/client/runtime/query_compiler_fast_bg.postgresql.wasm-base64.mjs",
+    },
+  },
 };
 
 export default nextConfig;
